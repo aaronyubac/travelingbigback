ca9af75c145cffdb08e295918567bedd3faec5eb
aaronyubac <aaronyubac@gmail.com>
2024-11-20 10:49:06 -0800

    predict word function based on inputted string

--- trie.js
+++ trie.js
@@ -32,9 +32,50 @@ class Trie {
 
     }
 
+    predictWord(string) {
+
+        // returns the root of a sub-tree
+        const getRemainingTree = function(string, root) {
+
+            let current = root;
+
+            // traverse up to last letter of string
+            for (const char of string) {
+                if (current.children.has(char)) {
+                    current = current.children.get(char);
+                }
+            }
+
+            return current;
+        };
+
+        var res = [];
+
+        // get all words from a given sub-tree
+        const getWords = function(string, root) {
+            
+            for (const child of root.children.values()) {
+                
+                const newString = string + child.char;
+                if (child.isWord) {
+                    res.push(newString);
+                }
+                
+                if(root.children.size === 0) {
+                    return
+                }
+
+                getWords(newString, child);
+            }
+
+        }
+
+
+        const start = getRemainingTree(string, this.root);
+        getWords(string, start);
+        console.log(res);
+
+
+    }
+
 }
-
-const trie = new Trie();
-
-trie.insert('fart');
-trie.insert('fact')
