1606367edb50da17937d4166afc207607ca6fb1a
aaronyubac <aaronyubac@gmail.com>
2024-11-25 21:13:57 -0800

    move tour logic to its own file and buildRoute() now adds places to a queue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 "use strict"
 import { Trie } from "/src/trie.js"
+import { buildTour } from "/src/tour.js"
 
 
 // SEARCHBAR SETUP
@@ -44,8 +45,6 @@ function updatePredictionList() {
 // front-end validation
 
 
-
-
 // TOUR SUBMISSION
 const tourForm = document.querySelector(".tour-form");
 tourForm.addEventListener("submit", function(e) {
@@ -55,206 +54,13 @@ tourForm.addEventListener("submit", function(e) {
 async function submitTour(e) {
     // Prevent form submission
     e.preventDefault();
+
+    // replace form.value's spaces with underscores
+    let query = searchBar.value.trim();
+    query = query.split(' ').join('_'); 
+
+    const tourQueue = await buildTour(query);
     
-    try {
-        const userCoords = await getUserLocation();
-
-        // replace form.value's spaces with underscores
-        let query = searchBar.value.trim();
-        query = query.split(' ').join('_'); 
-
-        // Get nearby locations(nodes)
-        const places = await nearbySearch(userCoords.latitude, userCoords.longitude, query);
-
-        // Get durations(edges - { destination, duration })
-        // const durations = await getDurations(userCoords.latitude, userCoords.longitude, places);
-        // const durationAdjacencyList = createAdjacencyList(durations);
-
-        
-       
-        buildRoute(durationAdjacencyList);
-        
-    } catch(err) {
-        console.log(err);
-    }
-    
-    
+    console.log("From main(): " + JSON.stringify(tourQueue.peek().place));
 }
 
-function getUserLocation() {
-    const showError = function(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                console.log("User denied the request for Geolocation.");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                console.log("Location information is unavailable.");
-                break;
-            case error.TIMEOUT:
-                console.log("The request to get user location timed out.");
-                break;
-            case error.UNKNOWN_ERROR:
-                console.log("An unknown error occurred.")
-                break;
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                resolve(position.coords);
-            }, showError);
-        } else {
-            console.log("Geolocation is not supported by this browser")
-            reject();
-        }
-
-    })
-
-}
-
-async function nearbySearch(lat, long, query) {
-
-        const { Place } = await google.maps.importLibrary(
-            "places",
-      );
-
-      // BUILD GOOGLE PLACES REQUEST
-      const buildRequest = function (lat, long, query) {
-          let center = new google.maps.LatLng(lat, long);
-          
-          const fields = ["displayName", "location", "formattedAddress", "id"];
-          const maxResultCount = 5;
-          
-          const request = {
-              fields: fields,
-              locationRestriction: {
-                  center: center,
-                  radius: 1000,
-                },
-                includedPrimaryTypes: [query],
-                maxResultCount: maxResultCount,
-        };
-        
-        return request;
-    }
-    
-    const request = buildRequest(lat, long, query);
-    // const { places } = await Place.searchNearby(request);
-    
-    const places = JSON.parse(`[{"id":"ChIJmRajZBNR2YAR5jQqf0-jQvM","displayName":"La Sinaloense Bar & Grill","formattedAddress":"954 Cardiff St, San Diego, CA 92114, USA"},{"id":"ChIJ8Z2zveRQ2YARSP82jljLOMU","displayName":"California Mexican Food","formattedAddress":"940 Cardiff St, San Diego, CA 92114, USA"},{"id":"ChIJgX-eqeZQ2YARMpx0bu-pk1I","displayName":"Pizzero Grill","formattedAddress":"1501 Skyline Dr, Lemon Grove, CA 91945, USA"}]`);
-
-    return places;
-}
-
-function getDurations(lat, long, places) {
-    
-    return new Promise(resolve => {
-
-        let center = new google.maps.LatLng(lat, long);
-        
-        const addresses = [center];
-        
-        for (const place of places) {
-            addresses.push(place.formattedAddress);
-        }
-
-        const travelMode = 'DRIVING';
-        const unitSystem = google.maps.UnitSystem.IMPERIAL;
-        const avoidTolls = true;
-    
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix(
-        {
-            origins: addresses,
-            destinations: addresses,
-            travelMode: travelMode,
-            unitSystem: unitSystem,
-            avoidTolls: avoidTolls,
-        }, (response, status) => {
-            resolve({ response, status })
-        });
-    })
-        
-}
-
-function createAdjacencyList({ response, status }) {
-    
-    class Edge {
-        constructor(destination, duration) {
-            this.destination = destination;
-            this.duration = duration;
-        }
-    }
-
-    if (status == 'OK') {
-        const origins = response.originAddresses;
-        const destinations = response.destinationAddresses;
-
-        const durationAdjacencyList = {};
-    
-        for (let i = 0; i < origins.length; i++) { 
-            const results = response.rows[i].elements;
-        
-            durationAdjacencyList[origins[i]] = [];
-
-            for (let j = 0; j < results.length; j++) { 
-                const duration = results[j].duration;
-
-                const edge = new Edge(destinations[j], duration);
-                durationAdjacencyList[origins[i]].push(edge);
-
-            }
-        }
-        return durationAdjacencyList;
-    } else {
-        // handle failed received response
-        console.log(status);
-    }
-
-} 
-
-function buildRoute(durationAdjacencyList) {
-        const visited = new Set();
-
-        let currentNode = Object.keys(durationAdjacencyList)[0];
-        console.log("currentNode: " + currentNode);
-
-        while(true) {
-            visited.add(currentNode);
-            console.log("visited: " + [...visited]);
-
-            const edges = durationAdjacencyList[currentNode];
-
-            let minimum;
-
-            for (const edge of edges) {
-
-                if(visited.has(edge.destination)) {
-                    continue;
-                }
-
-                // case: minimum has not been assigned yet
-                if(minimum === undefined) {
-                    minimum = edge;
-                    continue;
-                }
-
-                if(edge.duration.value < minimum.duration.value) {
-                    minimum = edge;
-                }
-            }
-
-            console.log("minimum: " + JSON.stringify(minimum));
-
-            // no more nodes left
-            if(minimum === undefined) {
-                break;
-            } else {
-                // add minimum to queue
-                currentNode = minimum.destination;
-            }
-
-        }
-
-        // return queue;
-}
---
+++ src/tour.js
@@ -0,0 +1,280 @@
+"use strict"
+export { buildTour }
+
+async function buildTour(query) {
+
+    try {
+        const userCoords = await getUserLocation();
+        
+        
+        // Get nearby locations(nodes)
+        const places = await nearbySearch(userCoords.latitude, userCoords.longitude, query);
+        
+        // Get durations(edges - { destination, duration })
+        const durations = await getDurations(userCoords.latitude, userCoords.longitude, places);
+        const durationAdjacencyList = createAdjacencyList(durations);
+        
+        const tourQueue = buildRoute(places, durationAdjacencyList);
+
+        return tourQueue;
+        
+    } catch(err) {
+        console.log(err);
+    }
+       
+}
+
+function getUserLocation() {
+    const showError = function(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                console.log("User denied the request for Geolocation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.log("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                console.log("The request to get user location timed out.");
+                break;
+            case error.UNKNOWN_ERROR:
+                console.log("An unknown error occurred.")
+                break;
+        }
+    }
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                resolve(position.coords);
+            }, showError);
+        } else {
+            console.log("Geolocation is not supported by this browser")
+            reject();
+        }
+
+    })
+
+}
+
+async function nearbySearch(lat, long, query) {
+
+        const { Place } = await google.maps.importLibrary(
+            "places",
+      );
+
+      // BUILD GOOGLE PLACES REQUEST
+      const buildRequest = function (lat, long, query) {
+          let center = new google.maps.LatLng(lat, long);
+          
+          const fields = ["displayName", "location", "formattedAddress", "id"];
+          const maxResultCount = 5;
+          
+          const request = {
+              fields: fields,
+              locationRestriction: {
+                  center: center,
+                  radius: 1000,
+                },
+                includedPrimaryTypes: [query],
+                maxResultCount: maxResultCount,
+        };
+        
+        return request;
+    }
+    
+    const request = buildRequest(lat, long, query);
+    const { places } = await Place.searchNearby(request);
+    
+    return places;
+}
+
+function getDurations(lat, long, places) {
+    
+    return new Promise(resolve => {
+
+        let center = new google.maps.LatLng(lat, long);
+        
+        const addresses = [center];
+        
+        for (const place of places) {
+            addresses.push(place.formattedAddress);
+        }
+
+        const travelMode = 'DRIVING';
+        const unitSystem = google.maps.UnitSystem.IMPERIAL;
+        const avoidTolls = true;
+    
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+        {
+            origins: addresses,
+            destinations: addresses,
+            travelMode: travelMode,
+            unitSystem: unitSystem,
+            avoidTolls: avoidTolls,
+        }, (response, status) => {
+            resolve({ response, status })
+        });
+    })
+        
+}
+
+function createAdjacencyList({ response, status }) {
+    
+    class Edge {
+        constructor(destination, duration) {
+            this.destination = destination;
+            this.duration = duration;
+        }
+    }
+
+    if (status == 'OK') {
+        const origins = response.originAddresses;
+        const destinations = response.destinationAddresses;
+
+        const durationAdjacencyList = {};
+    
+        for (let i = 0; i < origins.length; i++) { 
+            const results = response.rows[i].elements;
+        
+            durationAdjacencyList[origins[i]] = [];
+
+            for (let j = 0; j < results.length; j++) { 
+                const duration = results[j].duration;
+
+                const edge = new Edge(destinations[j], duration);
+                durationAdjacencyList[origins[i]].push(edge);
+
+            }
+        }
+        return durationAdjacencyList;
+    } else {
+        // handle failed received response
+        console.log(status);
+    }
+
+} 
+
+function buildRoute(places, durationAdjacencyList) {
+        const visited = new Set();
+        const tourQueue = new TourQueue();
+
+        const placeMap = new Map();
+        for (const place of places) {
+            placeMap.set(place.formattedAddress, place);
+        }
+
+        let currentNode = Object.keys(durationAdjacencyList)[0];
+        console.log("currentNode: " + currentNode);
+
+        while(true) {
+            visited.add(currentNode);
+
+            const edges = durationAdjacencyList[currentNode];
+
+            let minimum;
+
+            for (const edge of edges) {
+
+                if(visited.has(edge.destination)) {
+                    continue;
+                }
+
+                // case: minimum has not been assigned yet
+                if(minimum === undefined) {
+                    minimum = edge;
+                    continue;
+                }
+
+                if(edge.duration.value < minimum.duration.value) {
+                    minimum = edge;
+                }
+            }
+
+            console.log("minimum: " + JSON.stringify(minimum));
+
+            // no more nodes left
+            if(minimum === undefined) {
+                break;
+            } else {
+                // add place to queue
+                const place = placeMap.get(minimum.destination);
+                tourQueue.enqueue(place);
+
+                // go to nearest place
+                currentNode = minimum.destination;
+            }
+
+        }
+
+        return tourQueue;
+}
+
+class TourQueueNode {
+    constructor(place) {
+        this.place = place;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class TourQueue {
+    constructor() {
+        this.front = null;
+        this.back = null;
+    }
+
+    enqueue(place) {
+        const newNode = new TourQueueNode(place);
+
+        // case: queue is empty
+        if(this.front === null) {
+            this.front = newNode;
+            this.back = newNode;
+        }
+
+        // regular case
+        newNode.prev = this.back
+        this.back.next = newNode;
+
+        this.back = newNode;
+    }
+
+    dequeue() {
+
+        const dequeued = this.front;
+
+        // case: one item in queue
+        if(this.front === this.back) {
+            this.front = null;
+            this.back = null;
+
+            return dequeued;
+        }
+
+        // regular case
+        next = this.front.next;
+
+        // remove pointers
+        this.front.next = null;
+        next.prev = null;
+
+        // change front
+        this.front = next;
+
+        return dequeued;
+
+    }
+
+    peek() {
+        return this.front;
+    }
+
+    print() {
+        let current = this.front;
+
+        while(current !== null) {
+            console.log(current.place);
+            current = current.next;
+        }
+    }
+}
